b2173adbaa91f8aca86b3199805881f8afe7fd04
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-08-31 23:41:13 -0300

    Alteracao layout cadastro de tarefa

--- app/src/main/java/com/asuprojects/tarefafeita/activity/TarefaActivity.java
+++ app/src/main/java/com/asuprojects/tarefafeita/activity/TarefaActivity.java
@@ -1,5 +1,6 @@
 package com.asuprojects.tarefafeita.activity;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatSpinner;
@@ -8,6 +9,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.applandeo.materialcalendarview.CalendarView;
@@ -22,14 +25,12 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-public class TarefaActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class TarefaActivity extends AppCompatActivity {
 
     private Button btnSelecaoData;
+    private Button btnSelecaoHorario;
 
     private Toolbar toolbar;
-    private AppCompatSpinner spinner;
-
-    private Prioridade[] values = Prioridade.values();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,14 +41,11 @@ public class TarefaActivity extends AppCompatActivity implements AdapterView.OnI
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        ArrayAdapter<Prioridade> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, values);
-        spinner = findViewById(R.id.spinner_prioridade);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
-
 
         btnSelecaoData = findViewById(R.id.btnCalendar);
+        Calendar data = Calendar.getInstance();
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        btnSelecaoData.setText(format.format(data.getTime()));
         btnSelecaoData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,7 +58,29 @@ public class TarefaActivity extends AppCompatActivity implements AdapterView.OnI
             }
         });
 
+        btnSelecaoHorario = findViewById(R.id.btnHorario);
+        int hora = data.get(Calendar.HOUR_OF_DAY);
+        int minuto = data.get(Calendar.MINUTE);
+        btnSelecaoHorario.setText(hora + ":" + minuto);
+        btnSelecaoHorario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar atual = Calendar.getInstance();
+                int horaAtual = atual.get(Calendar.HOUR_OF_DAY);
+                int minutosAtuais = atual.get(Calendar.MINUTE);
+                TimePickerDialog dialog = new TimePickerDialog(TarefaActivity.this, listenerHorario, horaAtual, minutosAtuais, true);
+                dialog.show();
+            }
+        });
+
     }
+
+    private TimePickerDialog.OnTimeSetListener listenerHorario = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker timePicker, int hora, int minutos) {
+            btnSelecaoHorario.setText(hora + ":" + minutos);
+        }
+    };
 
     private OnSelectDateListener listener = new OnSelectDateListener() {
         @Override
@@ -73,13 +93,25 @@ public class TarefaActivity extends AppCompatActivity implements AdapterView.OnI
         }
     };
 
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        Toast.makeText(this, "Selecao: " + values[i], Toast.LENGTH_SHORT).show();
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch(view.getId()){
+            case R.id.item_baixa:
+                if(checked){
+                    Toast.makeText(this, "BAIXA selecionada", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.item_media:
+                if(checked){
+                    Toast.makeText(this, "MEDIA selecionada", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.item_alta:
+                if(checked){
+                    Toast.makeText(this, "ALTA selecionada", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-
-    }
 }
